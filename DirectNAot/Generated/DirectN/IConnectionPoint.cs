bbc84419ab7d8d7fcc9e5155eaf150b76bc2330d
Simon Mourier <smourier@users.noreply.github.com>
2024-04-25 12:41:23 +0200

    Added connection points

---
+++ DirectNAot/Generated/DirectN/IConnectionPoint.cs
@@ -0,0 +1,32 @@
+﻿namespace DirectN;
+
+// https://learn.microsoft.com/windows/win32/api/ocidl/nn-ocidl-iconnectionpoint
+[SupportedOSPlatform("windows5.0")]
+[GeneratedComInterface, Guid("b196b286-bab4-101a-b69c-00aa00341d07")]
+public partial interface IConnectionPoint
+{
+    // https://learn.microsoft.com/windows/win32/api/ocidl/nf-ocidl-iconnectionpoint-getconnectioninterface
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT GetConnectionInterface(out Guid pIID);
+    
+    // https://learn.microsoft.com/windows/win32/api/ocidl/nf-ocidl-iconnectionpoint-getconnectionpointcontainer
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT GetConnectionPointContainer(out IConnectionPointContainer ppCPC);
+    
+    // https://learn.microsoft.com/windows/win32/api/ocidl/nf-ocidl-iconnectionpoint-advise
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT Advise(nint pUnkSink, out uint pdwCookie);
+    
+    // https://learn.microsoft.com/windows/win32/api/ocidl/nf-ocidl-iconnectionpoint-unadvise
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT Unadvise(uint dwCookie);
+    
+    // https://learn.microsoft.com/windows/win32/api/ocidl/nf-ocidl-iconnectionpoint-enumconnections
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT EnumConnections(out IEnumConnections ppEnum);
+}
---
+++ DirectNAot/Generated/DirectN/IEnumConnections.cs
@@ -0,0 +1,27 @@
+﻿namespace DirectN;
+
+// https://learn.microsoft.com/windows/win32/api/ocidl/nn-ocidl-ienumconnections
+[SupportedOSPlatform("windows5.0")]
+[GeneratedComInterface, Guid("b196b287-bab4-101a-b69c-00aa00341d07")]
+public partial interface IEnumConnections
+{
+    // https://learn.microsoft.com/windows/win32/api/ocidl/nf-ocidl-ienumconnections-next
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT Next(uint cConnections, [MarshalUsing(CountElementName = nameof(cConnections))] out CONNECTDATA[] rgcd, out uint pcFetched);
+    
+    // https://learn.microsoft.com/windows/win32/api/ocidl/nf-ocidl-ienumconnections-skip
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT Skip(uint cConnections);
+    
+    // https://learn.microsoft.com/windows/win32/api/ocidl/nf-ocidl-ienumconnections-reset
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT Reset();
+    
+    // https://learn.microsoft.com/windows/win32/api/ocidl/nf-ocidl-ienumconnections-clone
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT Clone(out IEnumConnections ppEnum);
+}
