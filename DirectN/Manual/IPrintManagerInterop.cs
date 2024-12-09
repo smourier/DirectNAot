342f41f701a15ded9fafba0825e4ef8d53de5b87
Simon Mourier <smourier@users.noreply.github.com>
2024-12-09 19:23:57 +0100

    Small add

---
+++ DirectN/Manual/IPrintManagerInterop.cs
@@ -0,0 +1,14 @@
+﻿namespace DirectN;
+
+[SupportedOSPlatform("windows8.0")]
+[GeneratedComInterface, Guid("C5435A42-8D43-4E7B-A68A-EF311E392087")]
+public partial interface IPrintManagerInterop : IInspectable
+{
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT GetForWindow(HWND appWindow, in Guid riid, out nint printManager);
+
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT ShowPrintUIForWindowAsync(HWND appWindow, in Guid riid, out nint asyncOperation);
+}
