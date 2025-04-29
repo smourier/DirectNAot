d7a18c4f85d9151d873be3aafe25105f74563172
Simon Mourier <smourier@users.noreply.github.com>
2025-04-29 19:10:54 +0200

    wip

---
+++ DirectN/Generated/DirectN/FONT_WEIGHT.cs
@@ -0,0 +1,21 @@
+﻿#nullable enable
+namespace DirectN;
+
+public enum FONT_WEIGHT : uint
+{
+    FW_DONTCARE = 0,
+    FW_THIN = 100,
+    FW_EXTRALIGHT = 200,
+    FW_LIGHT = 300,
+    FW_NORMAL = 400,
+    FW_MEDIUM = 500,
+    FW_SEMIBOLD = 600,
+    FW_BOLD = 700,
+    FW_EXTRABOLD = 800,
+    FW_HEAVY = 900,
+    FW_ULTRALIGHT = 200,
+    FW_REGULAR = 400,
+    FW_DEMIBOLD = 600,
+    FW_ULTRABOLD = 800,
+    FW_BLACK = 900,
+}
