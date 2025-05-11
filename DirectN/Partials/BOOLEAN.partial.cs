cf1a94803119f9e17766d255bdefc45cd99c0511
Simon Mourier <smourier@users.noreply.github.com>
2025-05-11 13:17:20 +0200

    wip

---
+++ DirectN/Partials/BOOLEAN.partial.cs
@@ -0,0 +1,12 @@
+﻿namespace DirectN;
+
+public partial struct BOOLEAN : IEquatable<BOOLEAN>
+{
+    public BOOLEAN(bool value)
+    {
+        Value = (byte)(value ? 1 : 0);
+    }
+
+    public static implicit operator BOOLEAN(bool result) => new(result);
+    public static implicit operator bool(BOOLEAN b) => b.Value != 0;
+}
