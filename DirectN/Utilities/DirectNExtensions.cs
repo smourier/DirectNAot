ef27a0242c3e95813b257385a10d90b1933aad31
Simon Mourier <smourier@users.noreply.github.com>
2025-05-23 13:26:40 +0200

    Improved clamp methods

--- DirectN/Utilities/DirectNExtensions.cs
+++ DirectN/Utilities/DirectNExtensions.cs
@@ -61,13 +61,47 @@ public static class DirectNExtensions
     public static int RoundI(this double value) => Math.Round(value).ToInt32();
     public static uint RoundU(this double value) => Math.Round(value).ToUInt32();
 
-    public static float Clamp(this float value, float min, float max = float.MaxValue) => value < min ? min : value > max ? max : value;
-    public static double Clamp(this double value, double min, double max = double.MaxValue) => value < min ? min : value > max ? max : value;
-    public static byte Clamp(this byte value, byte min, byte max = byte.MaxValue) => value < min ? min : value > max ? max : value;
-    public static int Clamp(this int value, int min, int max = int.MaxValue) => value < min ? min : value > max ? max : value;
-    public static uint Clamp(this uint value, uint min, uint max = uint.MaxValue) => value < min ? min : value > max ? max : value;
-    public static long Clamp(this long value, long min, long max = long.MaxValue) => value < min ? min : value > max ? max : value;
-    public static ulong Clamp(this ulong value, ulong min, ulong max = ulong.MaxValue) => value < min ? min : value > max ? max : value;
+    public static float Clamp(this float value, float min, float max = float.MaxValue)
+    {
+        if (max < min) (min, max) = (max, min);
+        return value < min ? min : value > max ? max : value;
+    }
+
+    public static double Clamp(this double value, double min, double max = double.MaxValue)
+    {
+        if (max < min) (min, max) = (max, min);
+        return value < min ? min : value > max ? max : value;
+    }
+
+    public static byte Clamp(this byte value, byte min, byte max = byte.MaxValue)
+    {
+        if (max < min) (min, max) = (max, min);
+        return value < min ? min : value > max ? max : value;
+    }
+
+    public static int Clamp(this int value, int min, int max = int.MaxValue)
+    {
+        if (max < min) (min, max) = (max, min);
+        return value < min ? min : value > max ? max : value;
+    }
+
+    public static uint Clamp(this uint value, uint min, uint max = uint.MaxValue)
+    {
+        if (max < min) (min, max) = (max, min);
+        return value < min ? min : value > max ? max : value;
+    }
+
+    public static long Clamp(this long value, long min, long max = long.MaxValue)
+    {
+        if (max < min) (min, max) = (max, min);
+        return value < min ? min : value > max ? max : value;
+    }
+
+    public static ulong Clamp(this ulong value, ulong min, ulong max = ulong.MaxValue)
+    {
+        if (max < min) (min, max) = (max, min);
+        return value < min ? min : value > max ? max : value;
+    }
 
     public static float ClampMinMax(this float value)
     {
