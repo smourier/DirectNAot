461f016c59bbb55eac9872283fa083d457c30391
Simon Mourier <smourier@users.noreply.github.com>
2025-06-22 14:54:38 +0200

    wip

---
+++ DirectN/Generated/DirectN/FDEX_PROP_FLAGS.cs
@@ -0,0 +1,21 @@
+﻿#nullable enable
+namespace DirectN;
+
+[Flags]
+public enum FDEX_PROP_FLAGS : uint
+{
+    fdexPropCanGet = 1,
+    fdexPropCannotGet = 2,
+    fdexPropCanPut = 4,
+    fdexPropCannotPut = 8,
+    fdexPropCanPutRef = 16,
+    fdexPropCannotPutRef = 32,
+    fdexPropNoSideEffects = 64,
+    fdexPropDynamicType = 128,
+    fdexPropCanCall = 256,
+    fdexPropCannotCall = 512,
+    fdexPropCanConstruct = 1024,
+    fdexPropCannotConstruct = 2048,
+    fdexPropCanSourceEvents = 4096,
+    fdexPropCannotSourceEvents = 8192,
+}
---
+++ DirectN/Generated/DirectN/IDispatchEx.cs
@@ -0,0 +1,38 @@
+﻿#nullable enable
+namespace DirectN;
+
+[GeneratedComInterface, Guid("a6ef9860-c720-11d0-9337-00a0c90dcaa9")]
+public partial interface IDispatchEx : IDispatch
+{
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT GetDispID(BSTR bstrName, uint grfdex, out int pid);
+    
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT InvokeEx(int id, uint lcid, ushort wFlags, in DISPPARAMS pdp, nint /* optional VARIANT* */ pvarRes, nint /* optional EXCEPINFO* */ pei, [MarshalUsing(typeof(UniqueComInterfaceMarshaller<IServiceProvider?>))] IServiceProvider? pspCaller);
+    
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT DeleteMemberByName(BSTR bstrName, uint grfdex);
+    
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT DeleteMemberByDispID(int id);
+    
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT GetMemberProperties(int id, uint grfdexFetch, out FDEX_PROP_FLAGS pgrfdex);
+    
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT GetMemberName(int id, out BSTR pbstrName);
+    
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT GetNextDispID(uint grfdex, int id, out int pid);
+    
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT GetNameSpaceParent(out nint ppunk);
+}
