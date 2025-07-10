8175bd47898b7b9ae2a23310e90a299da22536da
Simon Mourier <smourier@users.noreply.github.com>
2025-07-10 12:06:48 +0200

    wip

---
+++ DirectN/Generated/DirectN/WINDOWPOS.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+namespace DirectN;
+
+// https://learn.microsoft.com/windows/win32/api/winuser/ns-winuser-windowpos
+public partial struct WINDOWPOS
+{
+    public HWND hwnd;
+    public HWND hwndInsertAfter;
+    public int x;
+    public int y;
+    public int cx;
+    public int cy;
+    public SET_WINDOW_POS_FLAGS flags;
+}
