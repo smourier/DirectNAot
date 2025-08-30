2361e491bb26a7c2895f3c5ed5c355373e1f5c1c
Simon Mourier <smourier@users.noreply.github.com>
2025-08-30 12:06:33 +0200

    wip

---
+++ DirectN/Generated/DirectN/PICTYPE.cs
@@ -0,0 +1,13 @@
+﻿#nullable enable
+namespace DirectN;
+
+// https://learn.microsoft.com/windows/win32/com/pictype-constants
+public enum PICTYPE : short
+{
+    PICTYPE_UNINITIALIZED = -1,
+    PICTYPE_NONE = 0,
+    PICTYPE_BITMAP = 1,
+    PICTYPE_METAFILE = 2,
+    PICTYPE_ICON = 3,
+    PICTYPE_ENHMETAFILE = 4,
+}
---
+++ DirectN/Partials/VARIANT_BOOL.partial.cs
@@ -0,0 +1,15 @@
+﻿namespace DirectN;
+
+public partial struct VARIANT_BOOL
+{
+    public static readonly BOOL VARIANT_TRUE = new(true);
+    public static readonly BOOL VARIANT_FALSE = new();
+
+    public VARIANT_BOOL(bool value)
+    {
+        Value = value ? (short)-1 : (short)0;
+    }
+
+    public static implicit operator VARIANT_BOOL(bool result) => new(result);
+    public static implicit operator bool(VARIANT_BOOL b) => b.Value != 0;
+}
