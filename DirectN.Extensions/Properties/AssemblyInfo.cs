3a21de7166195f93cf2f820e0edf7aa12b142b71
Simon Mourier <smourier@users.noreply.github.com>
2025-09-12 16:45:56 +0200

    Refactored build configuration.

--- DirectN.Extensions/Properties/AssemblyInfo.cs
+++ DirectN.Extensions/Properties/AssemblyInfo.cs
@@ -1,15 +1,4 @@
-﻿#if DEBUG
-[assembly: AssemblyConfiguration("DEBUG")]
-#else
-[assembly: AssemblyConfiguration("RELEASE")]
-#endif
-
-[assembly: AssemblyTitle("DirectN.Extensions")]
-[assembly: AssemblyProduct("DirectN")]
-[assembly: AssemblyCopyright("Copyright (C) 2017-2025 Simon Mourier. All rights reserved.")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyDescription("Support code for DirectN")]
-[assembly: AssemblyCompany("Simon Mourier")]
+﻿[assembly: AssemblyCulture("")]
 [assembly: Guid("952e737d-92da-4665-be06-f94f2fa90142")]
 [assembly: DisableRuntimeMarshalling]
 [assembly: SupportedOSPlatform("windows6.0.6000")]
