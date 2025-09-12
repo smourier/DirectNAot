dcf23f794b6f128e3ecd1d9d1218b3abd064c3ab
Simon Mourier <smourier@users.noreply.github.com>
2025-09-12 18:17:13 +0200

    Revert "Refactored build configuration."
    This reverts commit 3a21de7166195f93cf2f820e0edf7aa12b142b71.

--- DirectN.Extensions/Properties/AssemblyInfo.cs
+++ DirectN.Extensions/Properties/AssemblyInfo.cs
@@ -1,4 +1,15 @@
-﻿[assembly: AssemblyCulture("")]
+﻿#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#else
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
+
+[assembly: AssemblyTitle("DirectN.Extensions")]
+[assembly: AssemblyProduct("DirectN")]
+[assembly: AssemblyCopyright("Copyright (C) 2017-2025 Simon Mourier. All rights reserved.")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("Support code for DirectN")]
+[assembly: AssemblyCompany("Simon Mourier")]
 [assembly: Guid("952e737d-92da-4665-be06-f94f2fa90142")]
 [assembly: DisableRuntimeMarshalling]
 [assembly: SupportedOSPlatform("windows6.0.6000")]
