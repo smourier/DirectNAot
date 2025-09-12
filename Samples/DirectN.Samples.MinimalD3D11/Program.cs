4e0d0330f2803b308457c97c3e41c988e3aa54d2
Simon Mourier <smourier@users.noreply.github.com>
2025-09-12 18:48:54 +0200

    Refactored build configuration for samples.

--- Samples/DirectN.Samples.MinimalD3D11/Program.cs
+++ Samples/DirectN.Samples.MinimalD3D11/Program.cs
@@ -5,7 +5,7 @@ internal static class Program
     static void Main()
     {
         using var app = new Application();
-        using var win = new SampleWindow($"minimal d3d11 pt3 by d7samurai - DirectN AOT with .NET {Environment.Version}");
+        using var win = new SampleWindow($"minimal d3d11 pt3 by d7samurai - On {RuntimeInformation.ProcessArchitecture} WinUI3 and .NET {Environment.Version} AOT");
         win.ResizeClient(1000, 1000);
         win.Center();
         win.Show();
