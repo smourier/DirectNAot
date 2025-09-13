39586f347b4750859e9a88972aac245ec9e16460
Simon Mourier <smourier@users.noreply.github.com>
2025-09-13 05:46:39 +0200

    wip

--- DirectN.Extensions/Com/ComError.cs
+++ DirectN.Extensions/Com/ComError.cs
@@ -5,32 +5,34 @@ public static class ComError
     public static void SetError(string description, [CallerMemberName] string? source = null)
     {
         ArgumentNullException.ThrowIfNull(description);
-        Functions.CreateErrorInfo(out var info).ThrowOnError();
-        info.SetDescription(PWSTR.From(description)).ThrowOnError();
+        Functions.CreateErrorInfo(out var infoObj).ThrowOnError();
+        using var info = new ComObject<ICreateErrorInfo>(infoObj);
+        info.Object.SetDescription(PWSTR.From(description)).ThrowOnError();
         if (source != null)
         {
-            info.SetSource(PWSTR.From(source)).ThrowOnError();
+            info.Object.SetSource(PWSTR.From(source)).ThrowOnError();
         }
 
-        var ei = (IErrorInfo)info;
-        Functions.SetErrorInfo(0, ei).ThrowOnError();
+        var ei = info.As<IErrorInfo>()!;
+        Functions.SetErrorInfo(0, ei.Object).ThrowOnError();
     }
 
     public static Exception? GetError()
     {
-        Functions.GetErrorInfo(0, out var info);
-        if (info == null)
+        Functions.GetErrorInfo(0, out var infoObj);
+        if (infoObj == null)
             return null;
 
-        info.GetDescription(out var description);
+        using var info = new ComObject<IErrorInfo>(infoObj);
+        info.Object.GetDescription(out var description);
         var descString = description.ToString();
         BSTR.Dispose(ref description);
 
-        info.GetSource(out var source);
+        info.Object.GetSource(out var source);
         var sourceString = source.ToString();
         BSTR.Dispose(ref source);
 
-        info.GetHelpFile(out var help);
+        info.Object.GetHelpFile(out var help);
         var helpString = help.ToString();
         BSTR.Dispose(ref help);
 
