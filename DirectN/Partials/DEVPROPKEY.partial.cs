c38050d0f4ca0911a50651cfe1acff0a1ebe7ccb
Simon Mourier <smourier@users.noreply.github.com>
2025-10-08 16:55:54 +0200

    wip

--- DirectN/Partials/DEVPROPKEY.partial.cs
+++ DirectN/Partials/DEVPROPKEY.partial.cs
@@ -1,7 +1,9 @@
 ﻿namespace DirectN;
 
-public partial struct DEVPROPKEY
+public partial struct DEVPROPKEY : IEquatable<DEVPROPKEY>, IEquatable<PROPERTYKEY>
 {
+    public static readonly DEVPROPKEY Null = new();
+
     public DEVPROPKEY(Guid fmtid, uint pid)
     {
         this.fmtid = fmtid;
@@ -14,9 +16,37 @@ public partial struct DEVPROPKEY
         if (bytes.Length != 20)
             throw new ArgumentException(null, nameof(bytes));
 
-        fmtid = new Guid(bytes.Take(16).ToArray());
+        fmtid = new Guid([.. bytes.Take(16)]);
         pid = BitConverter.ToUInt32(bytes, 16);
     }
+
+    public readonly bool IsNull => pid == 0 && fmtid == Guid.Empty;
+
+    [SupportedOSPlatform("windows5.1.2600")]
+    public static DEVPROPKEY? FromString(string? value, bool throwOnError = false)
+    {
+        if (value == null)
+            return null;
+
+        var hr = Functions.PSPropertyKeyFromString(PWSTR.From(value), out var pk).ThrowOnError(throwOnError);
+        if (hr.IsError)
+            return null;
+
+        return new DEVPROPKEY(pk.fmtid, pk.pid);
+    }
+
+    public override readonly bool Equals(object? obj) => (obj is DEVPROPKEY value && Equals(value)) || (obj is PROPERTYKEY pk && Equals(pk));
+    public readonly bool Equals(DEVPROPKEY other) => other.pid == pid && other.fmtid == fmtid;
+    public readonly bool Equals(PROPERTYKEY other) => other.pid == pid && other.fmtid == fmtid;
+    public override readonly int GetHashCode() => fmtid.GetHashCode() ^ pid.GetHashCode();
+    public static bool operator ==(DEVPROPKEY left, DEVPROPKEY right) => left.Equals(right);
+    public static bool operator !=(DEVPROPKEY left, DEVPROPKEY right) => !left.Equals(right);
+
+    public static implicit operator DEVPROPKEY(PROPERTYKEY pk) => new(pk.fmtid, pk.pid);
+    public static explicit operator PROPERTYKEY(DEVPROPKEY pk) => new(pk.fmtid, pk.pid);
+
+    // see https://msdn.microsoft.com/en-us/library/windows/desktop/bb762085.aspx for official format
+    public override readonly string ToString() => $"{fmtid:B} {pid}";
 
     public static implicit operator DEVPROPKEY(byte[] value) => new(value);
 }
--- DirectN/Partials/PROPERTYKEY.partial.cs
+++ DirectN/Partials/PROPERTYKEY.partial.cs
@@ -1,6 +1,6 @@
 ﻿namespace DirectN;
 
-public partial struct PROPERTYKEY : IEquatable<PROPERTYKEY>
+public partial struct PROPERTYKEY : IEquatable<PROPERTYKEY>, IEquatable<DEVPROPKEY>
 {
     public static readonly PROPERTYKEY Null = new();
     public const uint PID_FIRST_USABLE = 2;
@@ -9,6 +9,16 @@ public partial struct PROPERTYKEY : IEquatable<PROPERTYKEY>
     {
         this.fmtid = fmtid;
         this.pid = pid;
+    }
+
+    public PROPERTYKEY(byte[] bytes)
+    {
+        ArgumentNullException.ThrowIfNull(bytes);
+        if (bytes.Length != 20)
+            throw new ArgumentException(null, nameof(bytes));
+
+        fmtid = new Guid([.. bytes.Take(16)]);
+        pid = BitConverter.ToUInt32(bytes, 16);
     }
 
     public readonly bool IsNull => pid == 0 && fmtid == Guid.Empty;
@@ -20,15 +30,24 @@ public partial struct PROPERTYKEY : IEquatable<PROPERTYKEY>
             return null;
 
         var hr = Functions.PSPropertyKeyFromString(PWSTR.From(value), out var pk).ThrowOnError(throwOnError);
-        return hr.IsError ? null : pk;
+        if (hr.IsError)
+            return null;
+
+        return pk;
     }
 
-    public override readonly bool Equals(object? obj) => obj is PROPERTYKEY value && Equals(value);
+    public override readonly bool Equals(object? obj) => (obj is DEVPROPKEY value && Equals(value)) || (obj is PROPERTYKEY pk && Equals(pk));
     public readonly bool Equals(PROPERTYKEY other) => other.pid == pid && other.fmtid == fmtid;
+    public readonly bool Equals(DEVPROPKEY other) => other.pid == pid && other.fmtid == fmtid;
     public override readonly int GetHashCode() => fmtid.GetHashCode() ^ pid.GetHashCode();
     public static bool operator ==(PROPERTYKEY left, PROPERTYKEY right) => left.Equals(right);
     public static bool operator !=(PROPERTYKEY left, PROPERTYKEY right) => !left.Equals(right);
 
+    public static explicit operator PROPERTYKEY(DEVPROPKEY pk) => new(pk.fmtid, pk.pid);
+    public static implicit operator DEVPROPKEY(PROPERTYKEY pk) => new(pk.fmtid, pk.pid);
+
     // see https://msdn.microsoft.com/en-us/library/windows/desktop/bb762085.aspx for official format
     public override readonly string ToString() => $"{fmtid:B} {pid}";
+
+    public static implicit operator PROPERTYKEY(byte[] value) => new(value);
 }
