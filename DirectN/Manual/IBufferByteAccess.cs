7d5862a3f1294c1a61afad2d5320ddfc34ddd748
Simon Mourier <simon_mourier@hotmail.com>
2026-05-07 18:29:46 +0200

    wip

---
+++ DirectN/Manual/IBufferByteAccess.cs
@@ -0,0 +1,9 @@
+﻿namespace DirectN;
+
+[GeneratedComInterface, Guid("905a0fef-bc53-11df-8c49-001e4fc686da")]
+public partial interface IBufferByteAccess
+{
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT Buffer(out nint buffer);
+}
---
+++ DirectN/Manual/IMemoryBufferByteAccess.cs
@@ -0,0 +1,9 @@
+﻿namespace DirectN;
+
+[GeneratedComInterface, Guid("5b0d3235-4dba-4d44-865e-8f1d0e4fd04d")]
+public partial interface IMemoryBufferByteAccess
+{
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT GetBuffer(out nint buffer, out uint capacity);
+}
